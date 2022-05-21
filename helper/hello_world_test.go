79483e3712c5bc77f9b4215c312f409da4e78ff0
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-05-21 13:37:19 +0700

    add before and after test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,32 @@
 package helper
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMain(m *testing.M) {
+	//Before Test
+	fmt.Println("Before Test")
+	//Run Test
+	m.Run()
+	//After Test
+	fmt.Println("After Test")
+}
+
 func TestHelloWorld(t *testing.T) {
 	assert.Equal(t, "Hello, World!", HelloWorld("World"))
+}
+
+func TestHelloDani(t *testing.T) {
+	assert.Equal(t, "Hello, Dani!", HelloWorld("Dani"))
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("skipping test")
+	}
 }
